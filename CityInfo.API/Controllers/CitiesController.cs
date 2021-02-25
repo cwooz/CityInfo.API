0276f706f88e2a528cecdd739a0554cb7c387b9f
Chris Woosley <woosley.christopher@gmail.com>
2021-02-25 12:37:36 -0500

    basic routing

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
-    public class CitiesController
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
     {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id = 1, Name = "New York City" },
+                    new { id = 2, Name = "Antwerp" }
+                });
+        }
     }
 }
