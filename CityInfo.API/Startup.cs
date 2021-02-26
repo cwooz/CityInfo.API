41ea7c5ca548777c46e355fd7b75991f7383cc26
Chris Woosley <woosley.christopher@gmail.com>
2021-02-26 11:42:28 -0500

    add optional xml response formatting

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using System;
@@ -16,14 +17,17 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });  
 
             //.AddJsonOptions(o =>          // SUPPORT OLDER SERIALIZTION (if needed)
             //{
             //    if (o.SerializerSettings.ContractResolver != null)
             //    {
             //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
-
             //        castedResolver.NamingStrategy = null;
             //    }
             //});
