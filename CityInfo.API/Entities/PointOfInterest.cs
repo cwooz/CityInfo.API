e20eb56f50e293d5f5f5382fceb090da8025d0d6
Chris Woosley <woosleyc@vystarcu.org>
2021-03-03 14:03:47 -0500

    create entity classes

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }          // Convention Based
+        public int CityId { get; set; }        // Explicitly define foreign key property on dependent class
+    }
+}
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -8,9 +8,7 @@ namespace CityInfo.API.Models
     public class CityDto
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
 
         public int NumberOfPointsOfInterest
